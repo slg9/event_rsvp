ce3519b3f1ef59673db7891a83586c6ce2157001
slg-squirrel <sebastien.l@squirrel.fr>
2024-09-29 14:53:22 +0400

    add modal form add attendee

--- app/rsvp/[code]/page.tsx
+++ app/rsvp/[code]/page.tsx
@@ -1,9 +1,9 @@
 import { GetEventByCode } from "@/app/db/queries/events"
-import styles from './rsvp.module.css'
-import ClickHere from "@/app/components/rsvp/ClickHere"
+import styles from '@/app/components/rsvp/rsvp.module.css'
+import RSVP from "@/app/components/rsvp/RSVP"
 
-export default async function Home({ params }: { params: { code: string } }) {
 
+export default async function Home({ params,searchParams }: { params: { code: string },searchParams: { open_form?: string } }) {
   const event = await GetEventByCode(params.code)
   if (event.length === 0 || (event.length > 0 && !event[0].canva_url)) {
     return <div>not found</div>
@@ -13,11 +13,9 @@ export default async function Home({ params }: { params: { code: string } }) {
       <iframe loading="lazy" className={styles.iframe}
         src={event[0].canva_url} >
       </iframe>
-      <div style={{position:"absolute",right:20,top:20,display:"flex",flexDirection:"column", alignItems:"center"}}>
-        <div className={styles.btn_rsvp} >RSVP</div>
-          <ClickHere />
-      </div>
+      <RSVP id={event[0].id}/>
 
+      
     </div>)
 }
 
